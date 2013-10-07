dba12dad87694b4d839f3e7cf2f5a113ab0442c0
Karl Hobley <karlhobley10@gmail.com>
2013-10-07 23:24:11 +0100

    Added more urls

--- monitoring/views.py
+++ monitoring/views.py
@@ -12,9 +12,25 @@ class MonitorDetailView(DetailView):
     model = models.Monitor
 
 
+class CheckListView(ListView):
+    model = models.Check
+
+
+class CheckDetailView(DetailView):
+    model = models.Check
+    
+
 class ProblemListView(ListView):
     model = models.Problem
 
 
 class ProblemDetailView(DetailView):
     model = models.Problem
+
+
+class ReportListView(ListView):
+    model = models.Report
+
+
+class ReportDetailView(DetailView):
+    model = models.Report
